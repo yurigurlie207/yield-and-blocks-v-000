409e020842f4ef975209d03e7e016ecca6a9e483
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 20:39:55 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  if array != nil then
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!\n"
+  end
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
